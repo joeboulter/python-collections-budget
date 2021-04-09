1db32af4b4205912de40341b5a7f96a7eb6b0007
joeboulter <joe.boulter@gmail.com>
2021-04-09 09:26:55 +0100

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -6,7 +6,6 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
 
     divided_for_loop = expenses.categorize_for_loop()
-
     divided_set_comp = expenses.categorize_set_comprehension()
 
     if not divided_for_loop == divided_set_comp:
